71963ce1a0c8dcdd0c613eb9be076fb30669f558
GoetzDeBouville <31970713+GoetzDeBouville@users.noreply.github.com>
2023-11-21 23:15:40 +0300

    added utils

---
+++ app/src/main/java/com/hellcorp/restquest/ui/RootActivity.kt
@@ -0,0 +1,14 @@
+package com.hellcorp.restquest.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hellcorp.restquest.databinding.ActivityRootBinding
+
+class RootActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityRootBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/hellcorp/restquest/utils/Tools.kt
@@ -0,0 +1,58 @@
+package com.hellcorp.restquest.utils
+
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.hellcorp.restquest.R
+
+object Tools {
+    const val CLICK_DEBOUNCE_DELAY_500_MS = 500L
+
+    fun isBackgroundColorLight(color: Int): Boolean {
+        val red = Color.red(color)
+        val green = Color.green(color)
+        val blue = Color.blue(color)
+        val luminance = (0.2126 * red + 0.7152 * green + 0.0722 * blue).toFloat()
+        return luminance > 160
+    }
+
+    fun showSnackbar(
+        view: View,
+        message: String,
+        context: Context
+    ) {
+        val snackbar = Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
+        val snackTextColor = ContextCompat.getColor(context, R.color.white)
+        val backgroundColor = ContextCompat.getColor(context, R.color.black)
+
+        val textView =
+            snackbar.view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text)
+        textView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+        textView.textSize = 16f
+        textView.setTextColor(snackTextColor)
+        snackbar.view.setBackgroundColor(backgroundColor)
+        snackbar.show()
+    }
+
+    fun vibroManager(context: Context, duration: Long) {
+        val vibrationEffect =
+            VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val vibratorManager =
+                context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            vibratorManager.defaultVibrator.vibrate(vibrationEffect)
+        } else {
+            @Suppress("DEPRECATION")
+            val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            vibrator.vibrate(vibrationEffect)
+        }
+    }
+}
