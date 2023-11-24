299472039a7e7c1c868aff0d6323a4e61e53d214
GoetzDeBouville <31970713+GoetzDeBouville@users.noreply.github.com>
2023-11-24 12:19:55 +0300

    add state observer (data, empty list, progress bar)

--- app/src/main/java/com/hellcorp/restquest/ui/room/RoomFragment.kt
+++ app/src/main/java/com/hellcorp/restquest/ui/room/RoomFragment.kt
@@ -4,15 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hellcorp.restquest.databinding.FragmentRoomBinding
+import com.hellcorp.restquest.domain.network.models.RoomResponseState
+import com.hellcorp.restquest.domain.room.Room
 import com.hellcorp.restquest.ui.room.adapter.RoomsAdapter
 import com.hellcorp.restquest.utils.BindingFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RoomFragment : BindingFragment<FragmentRoomBinding>() {
     private val viewModel: RoomViewModel by viewModel()
-//    val adapter = RoomsAdapter(myRoomList)
-//    binding.rvRooms.adapter = adapter
 
     override fun createBinding(
         inflater: LayoutInflater,
@@ -23,6 +24,60 @@ class RoomFragment : BindingFragment<FragmentRoomBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        liveDataObserver()
+
+        viewModel.getRoomList()
+
     }
 
+    private fun liveDataObserver() {
+        viewModel.state.observe(viewLifecycleOwner) {
+            renderState(it)
+        }
+    }
+
+    private fun renderState(state: RoomResponseState) {
+        when (state) {
+            is RoomResponseState.Content -> showData(state.roomList)
+            is RoomResponseState.Empty -> showError()
+            RoomResponseState.Loading -> showLoading()
+            else -> showError()
+        }
+    }
+
+    private fun showData(roomList: List<Room>) {
+        val adapter = RoomsAdapter(roomList)
+        binding.rvRooms.adapter = adapter
+        binding.rvRooms.layoutManager = LinearLayoutManager(context)
+        updateUi(
+            contentVisibility = View.VISIBLE,
+            errorVisibility = View.GONE,
+            progressBarVisibility = View.GONE
+        )
+    }
+
+    private fun showError() {
+        updateUi(
+            contentVisibility = View.GONE,
+            errorVisibility = View.VISIBLE,
+            progressBarVisibility = View.GONE
+        )
+    }
+
+    private fun showLoading() {
+        updateUi(
+            contentVisibility = View.GONE,
+            errorVisibility = View.GONE,
+            progressBarVisibility = View.VISIBLE
+        )
+    }
+
+    private fun updateUi(contentVisibility: Int, errorVisibility: Int, progressBarVisibility: Int) =
+        with(binding) {
+            nsvRooms.visibility = contentVisibility
+            ivError.visibility = errorVisibility
+            tvError.visibility = errorVisibility
+            progressBar.visibility = progressBarVisibility
+        }
 }
